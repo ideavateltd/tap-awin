f71953eb717a47b6f5f7c8a5b0791ffe5a93c970
Hugh Nimmo-Smith <hugh@nimmo-smith.com>
2017-10-23 21:48:37 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-awin',
-      version='0.0.1',
+      version='0.0.2',
       description='Singer.io tap for extracting data from the Affiliate Window API',
       author='Onedox',
       url='https://github.com/onedox/tap-awin',
